a69129966eb307c85d7fc0fe648440d9dd5c5ccc
techysiddhant <sidjain8000@gmail.com>
2024-08-07 14:54:38 +0530

    add auto refresh token

--- src/layouts/Root.tsx
+++ src/layouts/Root.tsx
@@ -3,6 +3,7 @@ import { self } from "../http/api";
 import { useQuery } from "@tanstack/react-query";
 import { useAuthStore } from "../store";
 import { useEffect } from "react";
+import { AxiosError } from "axios";
 const getSelf = async () => {
     const { data } = await self();
     return data;
@@ -12,6 +13,12 @@ const Root = () => {
     const { data: selfData, isLoading } = useQuery({
         queryKey: ['self'],
         queryFn: getSelf,
+        retry: (failureCount: number, error) => {
+            if (error instanceof AxiosError && error.response?.status === 401) {
+                return false;
+            }
+            return failureCount < 3
+        }
     });
     useEffect(() => {
         if (selfData) {
