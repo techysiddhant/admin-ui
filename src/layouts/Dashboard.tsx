aa7dde3f0406d907e1fbe2e766fe91e0bb7d99f6
techysiddhant <sidjain8000@gmail.com>
2024-08-07 15:39:39 +0530

    add sidebar layout

--- src/layouts/Dashboard.tsx
+++ src/layouts/Dashboard.tsx
@@ -1,13 +1,73 @@
-import { Navigate, Outlet } from "react-router-dom"
+import { Navigate, NavLink, Outlet } from "react-router-dom"
 import { useAuthStore } from "../store"
-
+import { Layout, Menu, theme } from "antd";
+import Icon from '@ant-design/icons'
+import { useState } from "react";
+import Logo from "../components/icons/Logo";
+import Home from "../components/icons/Home";
+import UserIcon from "../components/icons/UserIcon";
+import { foodIcon } from "../components/icons/FoodIcon";
+import BasketIcon from "../components/icons/BasketIcon";
+import GiftIcon from "../components/icons/GiftIcon";
+const { Sider, Header, Content, Footer } = Layout;
+const items = [
+    {
+        key: '/',
+        icon: <Icon component={Home} />,
+        label: <NavLink to="/">Home</NavLink>
+    },
+    {
+        key: '/users',
+        icon: <Icon component={UserIcon} />,
+        label: <NavLink to="/users">Users</NavLink>
+    },
+    {
+        key: '/restaurants',
+        icon: <Icon component={foodIcon} />,
+        label: <NavLink to="/restaurants">Restaurants</NavLink>
+    },
+    {
+        key: '/products',
+        icon: <Icon component={BasketIcon} />,
+        label: <NavLink to="/users">Products</NavLink>
+    },
+    {
+        key: '/promos',
+        icon: <Icon component={GiftIcon} />,
+        label: <NavLink to="/users">Promos</NavLink>
+    }
+]
 const Dashboard = () => {
+    const [collapsed, setCollapsed] = useState(false);
+    const {
+        token: { colorBgContainer },
+    } = theme.useToken();
     const { user } = useAuthStore();
     if (user === null) {
         return <Navigate to='/auth/login' replace={true} />
     }
     return (
-        <Outlet />
+        <div className="">
+            <Layout style={{ minHeight: '100vh' }}>
+                <Sider theme="light" collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+                    <div className="logo">
+                        <Logo />
+                    </div>
+                    <Menu theme="light" defaultSelectedKeys={['/']} mode="inline" items={items} />
+                </Sider>
+                <Layout>
+                    <Header style={{ padding: 0, background: colorBgContainer }} />
+                    <Content style={{ margin: '0 16px' }}>
+
+                        <Outlet />
+                    </Content>
+                    <Footer style={{ textAlign: 'center' }}>
+                        MernSpace Pizza Shop ©{new Date().getFullYear()} Created by SID
+                    </Footer>
+                </Layout>
+            </Layout>
+
+        </div>
     )
 }
 
