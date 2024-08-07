1c9ce2fd778b8b50ca390022f46959de3f15af11
techysiddhant <sidjain8000@gmail.com>
2024-08-07 12:17:50 +0530

    add mutation for logout

--- src/Pages/login/login.tsx
+++ src/Pages/login/login.tsx
@@ -23,15 +23,21 @@ const LoginPage = () => {
         queryFn: getSelf,
         enabled: false
     });
-
+    const { mutate: logoutMutate } = useMutation({
+        mutationKey: ['logout'],
+        mutationFn: logout,
+        onSuccess: () => {
+            logoutFromStore();
+            return;
+        }
+    })
     const { mutate, isPending, isError, error } = useMutation({
         mutationKey: ['login'],
         mutationFn: loginUser,
         onSuccess: async () => {
             const { data: selfData } = await refetch();
             if (!isAllowed(selfData)) {
-                await logout();
-                logoutFromStore();
+                logoutMutate();
                 return;
             }
             setUser(selfData);
