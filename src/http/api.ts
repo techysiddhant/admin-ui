851c16e5a424f448125bca26850ea5459e3661df
techysiddhant <sidjain8000@gmail.com>
2024-09-04 17:01:37 +0530

    api gateway url fixes

--- src/http/api.ts
+++ src/http/api.ts
@@ -1,23 +1,25 @@
 import { CreateTenantData, CreateUserData, Credentials } from "../types";
 import { api } from "./client";
-
+export const AUTH_SERVICE = "/api/auth";
+// const CATALOG_SERVICE = "/api/auth";
 // Auth Service
 export const login = (credentials: Credentials) => {
-  return api.post("/auth/login", credentials);
+  return api.post(`${AUTH_SERVICE}/auth/login`, credentials);
 };
 export const self = () => {
-  return api.get("/auth/self");
+  return api.get(`${AUTH_SERVICE}/auth/self`);
 };
 export const logout = () => {
-  return api.post("/auth/logout");
+  return api.post(`${AUTH_SERVICE}/auth/logout`);
 };
 export const getUsers = (queryString: string) => {
-  return api.get(`/users?${queryString}`);
+  return api.get(`${AUTH_SERVICE}/users?${queryString}`);
 };
 export const getTenants = (queryString: string) =>
-  api.get(`/tenants?${queryString}`);
-export const createUser = (user: CreateUserData) => api.post(`/users`, user);
+  api.get(`${AUTH_SERVICE}/tenants?${queryString}`);
+export const createUser = (user: CreateUserData) =>
+  api.post(`${AUTH_SERVICE}/users`, user);
 export const createTenant = (tenant: CreateTenantData) =>
-  api.post(`/tenants`, tenant);
+  api.post(`${AUTH_SERVICE}/tenants`, tenant);
 export const updateUser = (user: CreateUserData, id: number) =>
-  api.patch(`/users/${id}`, user);
+  api.patch(`${AUTH_SERVICE}/users/${id}`, user);
